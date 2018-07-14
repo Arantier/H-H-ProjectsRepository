699f899ee8070ef2f68fda3e6a89cbd03516023d
ScherbakovDV <shcherbakov_dmitry@bk.ru>
2018-07-14 08:28:34 +0300

    Начал работу над 7 заданием

---
+++ SevenApp/app/src/main/java/ru/android_school/h_h/sevenapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.android_school.h_h.sevenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
